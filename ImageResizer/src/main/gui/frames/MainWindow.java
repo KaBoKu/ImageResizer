5bfdbdf068a9be887fecdac6a841ad36fdcbb1a9
Kamil Kus <kk.up.krk@gmail.com>
2014-03-20 10:32:48 +0100

    Make some fields

--- ImageResizer/src/main/gui/frames/MainWindow.java
+++ ImageResizer/src/main/gui/frames/MainWindow.java
@@ -1,7 +1,14 @@
 package main.gui.frames;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
 
 public class MainWindow extends JFrame{
-
+	private JButton chooseFileButton;
+	private JTextField nameOfFileField;
+	private JTextField widthField;
+	private JTextField heightField;
+	private JSpinner typeOfWatermarkJSpinner;
 }
